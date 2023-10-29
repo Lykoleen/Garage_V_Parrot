5029acb459cfb38fa17dfd9ea1d6c9e78d8d505b
Rabillard Tony <tony.rabillard16@gmail.com>
2023-10-29 22:23:06 +0100

    📦 NEW: Section présentation des services page accueil

--- src/Views/main/index.php
+++ src/Views/main/index.php
@@ -5,11 +5,31 @@
     <div class="container_presentation">
         <div class="content_presentation">
             <h1>GARAGE AUTOMOBILE <br> VINCENT PARROT</h1>
-            <p>Nous sommes ravis de vous accueillir sur notre site web, votre destination de confiance pour tous vos besoins en matière de véhicules. Que vous recherchiez des services d'entretien, de réparation, ou que vous souhaitiez acheter ou vendre une voiture, vous êtes au bon endroit.</p>
+            <p>Que vous recherchiez des services d'entretien, de réparation ou que souhaitiez acheter l'un de nos véhicules d'occasion entretenus par nos soins, notre équipe est là pour répondre à tous vos besoins avec expertise et dévouement.</p>
         </div>
         <a href="/annonces/index">
             <button class="w-50 btn btn-light btn_presentation">Voir les annonces</button>
         </a>
     </div>
     <div class="overlay"></div>
+</section>
+<section class="section_services">
+    <h1>NOS SERVICES</h1>
+    <div class="presentation_services">
+        <div class="card_services">
+            <img class="image_services" src="../assets/img/atelier.jpg" alt="photo de réparation d'un phare">
+            <h2>L'atelier</h2>
+            <p>Notre atelier est l'endroit idéal pour prendre soin de votre véhicule. Que vous ayez besoin d'un entretien régulier ou de réparations mécaniques sur votre voiture, notre équipe de professionnels qualifiés est là pour répondre à tous vos besoins.</p>
+        </div>
+        <div class="card_services">
+            <img class="image_services" src="../assets/img/tuto.jpg" alt="photo de deux personnes dans une voiture">
+            <h2>Nos conseils et tutos</h2>
+            <p>Nos experts dévoués sont là pour répondre à toutes vos questions, qu'il s'agisse d'entretien de routine, de dépannage en cas de panne, ou même de projets de personnalisation de votre voiture.</p>
+        </div>
+        <div class="card_services">
+            <img class="image_services" src="../assets/img/location.jpg" alt="photo d'un chien qui passe sa tête par la fenêtre de la voiture">
+            <h2>La location de véhicules</h2>
+            <p>Que vous ayez besoin d'un véhicule pour un voyage d'affaires, des vacances en famille ou tout simplement pour explorer de nouveaux horizons, nous avons la solution idéale pour vous.</p>
+        </div>
+    </div>
 </section>
